93c683a05d47925d07ad8abdebef6ed9f6dd58ed
Camilla Rodriguez <camntgg@gmail.com>
2026-06-29 03:51:19 -0400

    socket.ts modificacion

--- src/lib/socket.ts
+++ src/lib/socket.ts
@@ -2,8 +2,8 @@ import { io } from "socket.io-client";
 
 // Next.js usa 'process.env.NEXT_PUBLIC_...' o tu variable de entorno del .env
 // Ponemos un respaldo directo al puerto 4000 para asegurar el tiro en local
-const SOCKET_URL = process.env.REACT_APP_SOCKET_URL || "http://localhost:4000";
-
+//const SOCKET_URL = process.env.REACT_APP_SOCKET_URL || "http://localhost:4000";
+const SOCKET_URL = process.env.NEXT_PUBLIC_SOCKET_URL || 'http://localhost:4000' ;
 console.log("🔌 Intentando conectar el Socket a la URL:", SOCKET_URL);
 
 export const socket = io(SOCKET_URL, {
